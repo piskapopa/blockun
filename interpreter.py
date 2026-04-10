d83ce88292bfb7ff23a61189aae28b372a23abe1
piskapopa <spamaobnaruzitel@gmail.com>
2026-04-10 10:03:20 +0500

    сделал to

--- interpreter.py
+++ interpreter.py
@@ -17,14 +17,51 @@ def lex(c):
                     val = blocks[int(valb[1::])][0]
                 elif valb[0] == "f":
                     with open(valb[1::], "r", encoding="utf-8") as file:
-                        val = file.read()
+                        val = int(file.read())
                 elif valb == "stdout":
                     val = stdout
                 elif valb == "inp":
                     val = int(input())
                 elif valb.isdecimal():
-                    val = int(d)
+                    val = int(valb)
                 else:
-                    raise  BlockunError(f"SyntaxError: incorrect to syntax in string {num}")
+                    raise BlockunError(f"SyntaxError: incorrect to syntax in string {num}")
+                for i in dela:
+                    if i[0] == "b":
+                        if int(i[1::]) in blocks.keys():
+                            match blocks[int(i[1::])]:
+                                case "+":
+                                    blocks[int(i[1::])][0]+=val
+                                case "-":
+                                    blocks[int(i[1::])][0]-=val
+                                case "/":
+                                    blocks[int(i[1::])][0]//=val
+                                case "*":
+                                    blocks[int(i[1::])][0]*=val
+                                case "%":
+                                    blocks[int(i[1::])][0]%=val
+                                case "or":
+                                    blocks[int(i[1::])][0] = int(int(i[1::]) != 0 or val != 0)
+                                case "and":
+                                    blocks[int(i[1::])][0] = int(int(i[1::]) != 0 and val != 0)
+                                case "not":
+                                    blocks[int(i[1::])][0] = int(int(i[1::]) == 0)   
+                                case _:
+                                    raise BlockunError(f"BlockCorruptedError: uncorrect operation in block {int(i[1::])} and str {num}")
+                        else:
+                            blocks[int(i[1::])] = val
+                    if i[0] == "f":
+                        with open(i[1::], "a", encoding="utf-8") as opn:
+                            opn.write(val)
+                    if i == "stdout":
+                        print(val)
+                        stdout = val
+                    if i == "charout":
+                        print(chr(val))
+                        stdout = val
+                if valb[0] == "b":
+                    blocks[int(valb[1::])][0] = 0
+                if valb == "stdout":
+                    stdout = 0
         except IndexError:
             break
